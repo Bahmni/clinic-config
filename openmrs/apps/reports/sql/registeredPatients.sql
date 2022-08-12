b424ac44d5aa39de9079be87418b378cd2037697
Kavitha S <kavitha.s@thoughtworks.com>
2022-08-12 15:42:28 +0530

    BAH-1709 | refactored the date format from UTC to IST
    Co-authored-by: deepthi-m <deepthi.mantena@thoughtworks.com>

--- openmrs/apps/reports/sql/registeredPatients.sql
+++ openmrs/apps/reports/sql/registeredPatients.sql
@@ -1,10 +1,10 @@
 SELECT
-  (@rownum := @rownum + 1)                                  AS "Sr. No.",
-  pi.identifier    										                      AS "Patient Id",
-  concat(pn.given_name, " ", ifnull(pn.family_name, ""))    AS "Patient Name",
-  floor(DATEDIFF(NOW(), p.birthdate) / 365)                 AS "Age",
-  p.gender                                                  AS "Gender",
-  DATE_FORMAT(pt.date_created, "%d-%b-%Y")                  AS "Registration Date"
+  (@rownum := @rownum + 1)                                                   AS "Sr. No.",
+  pi.identifier    										                                       AS "Patient Id",
+  concat(pn.given_name, " ", ifnull(pn.family_name, ""))                     AS "Patient Name",
+  floor(DATEDIFF(NOW(), p.birthdate) / 365)                                  AS "Age",
+  p.gender                                                                   AS "Gender",
+  DATE_FORMAT(CONVERT_TZ(pt.date_created,'+00:00','+5:30'), "%d-%b-%Y")      AS "Registration Date"
 
 FROM patient pt
   JOIN person p ON p.person_id = pt.patient_id AND p.voided is FALSE
@@ -13,5 +13,5 @@ FROM patient pt
   JOIN person_name pn ON pn.person_id = p.person_id AND pn.voided is FALSE
   CROSS JOIN (SELECT @rownum := 0) AS dummy
   WHERE pt.voided is FALSE
-  AND cast(pt.date_created AS DATE) BETWEEN '#startDate#' AND '#endDate#'
+  AND cast(CONVERT_TZ(pt.date_created,'+00:00','+5:30') AS DATE) BETWEEN '#startDate#' AND '#endDate#'
 GROUP BY pi.identifier;
