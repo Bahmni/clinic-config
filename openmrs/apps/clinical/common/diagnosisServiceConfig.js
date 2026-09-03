f1c02b7382c56594371193628f80dd5ed535f8f4
dpkb-dev <deepsb.dpk@gmail.com>
2026-09-03 15:40:59 +0530

    BAH-2888: Fix address field labels on registration card print to match registration page

--- openmrs/apps/clinical/common/diagnosisServiceConfig.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-var Bahmni = Bahmni || {};
-Bahmni.Common = Bahmni.Common || {};
-Bahmni.Common.Constants = Bahmni.Common.Constants;
-Bahmni.Common.Constants.emrapiDiagnosisLimit = 200;
